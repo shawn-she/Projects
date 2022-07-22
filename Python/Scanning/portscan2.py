d545b3801860c37cdc541bf3c42ad589c356ebca
shawn-she <ritiktayal2000@hotmail.com>
2022-07-22 15:23:07 -0400

    python project vid3 from kali

--- Python/Scanning/portscan2.py
+++ Python/Scanning/portscan2.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python
 
 import socket
+from termcolor import colored
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # AF_INET = IPv4 address; SOCK_STREAM = TCP Packets
 socket.setdefaulttimeout(2)				# time in seconds
 
 host = input("[*] Enter the Host to Scan: ")		# takes string
-port = int(input("[*] Enter the Port to Scan: "))	# takes integer
-
 
 def portscanner(port):
 	if sock.connect_ex((host, port)):		# connect_ex looks for (any) error
-		print("Port %d is closed" %(port))	# if error found: port is closed
+		print(colored("[!!] Port %d is closed" %(port), 'red'))	# if error found: port is closed
 	else:
-		print("Port %d is open" %(port))	# else: port is open
+		print(colored("[+] Port %d is open" %(port), 'green'))	# else: port is open
 
-portscanner(port)
+for port in range(1, 1001):
+	portscanner(port)
