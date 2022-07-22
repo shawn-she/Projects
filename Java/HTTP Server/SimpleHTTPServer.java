142b9222e3d60641f80089d5f5c931d545f25e47
shawn-she <ritiktayal2000@hotmail.com>
2022-07-22 17:49:13 +0530

    updated java http server

--- Java/HTTP Server/SimpleHTTPServer.java
+++ Java/HTTP Server/SimpleHTTPServer.java
@@ -13,14 +13,23 @@ public class SimpleHTTPServer{
     public static void main(String[] args) throws Exception {
         final ServerSocket server = new ServerSocket(8080);
         System.out.println("Listening for connection on port 8080 ...");
+        // input
+        // while(true){
+        //     final Socket client = server.accept();
+        //     BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        //     String line = br.readLine();
+        //     while(!line.isEmpty()){
+        //         System.out.println(line);
+        //         line = br.readLine();
+        //     }
+        // }
+
+        // output
         while(true){
-            final Socket client = server.accept();
-            InputStreamReader isr = new InputStreamReader(client.getInputStream());
-            BufferedReader br = new BufferedReader(isr);
-            String line = br.readLine();
-            while(!line.isEmpty()){
-                System.out.println(line);
-                line = br.readLine();
+            try (Socket client = server.accept() ){
+                Date today = new Date();
+                String HTTPResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+                client.getOutputStream().write(HTTPResponse.getBytes("UTF-8"));
             }
         }
     }
